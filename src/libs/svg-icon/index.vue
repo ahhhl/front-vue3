ca9ef478abe6c9894601d7a346195ee7f01e13cf
ahhh <1208484386@qq.com>
2022-10-08 22:11:20 +0800

    添加svg-icons以及images

--- src/libs/svg-icon/index.vue
+++ src/libs/svg-icon/index.vue
@@ -1,7 +1,27 @@
-import svgIcon from './svg-icon/index.vue';
+<template>
+  <svg aria-hidden="true">
+    <use :class="fillClass" :href="symbolId" :fill="color" />
+  </svg>
+</template>
 
-export default {
-  install(app) {
-    app.component('m-svg-icon', svgIcon);
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  // 显示的 svg 图标名称（剔除 icon-）
+  name: {
+    type: String,
+    required: true
+  },
+  // 直接指定 svg 图标的颜色
+  color: {
+    type: String
+  },
+  // 通过 tailwind 指定 svg 颜色的类名
+  fillClass: {
+    type: String
   }
-};
+});
+// 真实显示的 svg 图标名（拼接 #icon-）
+const symbolId = computed(() => `#icon-${props.name}`);
+</script>
