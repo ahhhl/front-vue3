3f78dddc56641692050face4ddd6093d18e70d26
ahhh <1208484386@qq.com>
2022-10-09 18:00:54 +0800

    menu组件实现

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 一级路由出口 -->
-  <div class="h-screen fixed top-0 left-0 w-screen">
+  <div>
     <router-view></router-view>
     <!-- <m-transition-router-view
       mainComponentName="home"
