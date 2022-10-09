c71fffeb63d2396dd705c64ca0a4a8b4b1156c14
ahhh <1208484386@qq.com>
2022-10-09 18:01:55 +0800

    通用组件自动化注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,7 +1,16 @@
-import svgIcon from './svg-icon/index.vue';
+// import svgIcon from './svg-icon/index.vue';
+// import popup from './popup/index.vue';
+import { defineAsyncComponent } from 'vue';
 
 export default {
   install(app) {
-    app.component('m-svg-icon', svgIcon);
+    // app.component('m-svg-icon', svgIcon);
+    // app.component('m-popup', popup);
+    const components = import.meta.glob('./*/index.vue');
+    for (const [fullPath, fn] of Object.entries(components)) {
+      const componentName = 'm-' + fullPath.replace('./', '').split('/')[0];
+      // defineAsyncComponent 定义一个异步组件，它在运行时是懒加载的。参数可以是一个异步加载函数，或是对加载行为进行更具体定制的一个选项对象。
+      app.component(componentName, defineAsyncComponent(fn));
+    }
   }
 };
