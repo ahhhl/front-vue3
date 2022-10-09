cfca0e732b717d2e3c792c3ce05e108ecc6c5928
ahhh <1208484386@qq.com>
2022-10-09 10:19:58 +0800

    移动端滑块处理

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -17,19 +17,25 @@
         <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
       </li>
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
         :key="item.name"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="{ 'text-zinc-100': currentCategoryIndex === index }"
         :ref="setItemRef"
+        @click="onItemClick(index)"
       >
         {{ item.name }}
       </li>
+      {{
+        ulScrollLeft
+      }}
     </ul>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { useScroll } from '@vueuse/core';
+import { onBeforeUpdate, ref, watch } from 'vue';
 
 defineProps({
   data: {
@@ -48,13 +54,34 @@ const sliderStyle = ref({
 let itemRefs = [];
 const currentCategoryIndex = ref(0);
 const setItemRef = (el) => {
-  console.log(el);
   if (el) {
     itemRefs.push(el);
   }
 };
 
 //数据改变之后, DOM 变化之前
+onBeforeUpdate(() => {
+  itemRefs = [];
+});
+
+// 获取ul元素
+const ulTarget = ref(null);
+// 通过vueuse -> useScroll 获取响应式的 scroll 滚动距离
+const { x: ulScrollLeft } = useScroll(ulTarget);
+
+// 监听currentCategoryIndex的变化
+watch(currentCategoryIndex, (val) => {
+  const { left, width } = itemRefs[val].getBoundingClientRect();
+  sliderStyle.value = {
+    // 滑块位置 = ul横向滚动距离 + 当前元素的left - ul的padding
+    transform: `translateX(${ulScrollLeft.value + left - 10}px)`,
+    width: width + 'px'
+  };
+});
+
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index;
+};
 </script>
 
 <style lang="scss" scoped></style>
