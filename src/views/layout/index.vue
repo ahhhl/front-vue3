c130f7c1744062062720724b48f533460ef29a8d
ahhh <1208484386@qq.com>
2022-10-20 17:44:52 +0800

    页面布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,9 +2,9 @@
   <div class="h-screen">
     <!-- 需要在 tailwind 中定义 h-header、h-main 高度 -->
     <header-vue class="h-header"></header-vue>
-    <div class="h-main">
-      <main-vue></main-vue>
-    </div>
+    <!-- <div class="h-main"> -->
+    <main-vue class="h-main"></main-vue>
+    <!-- </div> -->
     <floating-vue></floating-vue>
   </div>
 </template>
@@ -12,8 +12,7 @@
 <script setup>
 import headerVue from './components/header/index.vue';
 import mainVue from './components/main.vue';
-import floatingVue from './components/floating/index.vue';
-import {} from 'vue';
+import floatingVue from './components/floating.vue';
 </script>
 
 <style lang="scss" scoped></style>
