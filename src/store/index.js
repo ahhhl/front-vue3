7b5fac94885293971643078334355389b4e578e5
ahhh <1208484386@qq.com>
2022-11-03 10:38:01 +0800

    处理navigationBar闪烁问题

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import { createStore } from 'vuex';
 import getters from './getters';
 import category from './modules/category';
+import createPersistedState from 'vuex-persistedstate';
 
 const store = createStore({
   getters,
   modules: {
     category
-  }
+  },
+  plugins: [
+    createPersistedState({
+      // 指定保存到localStorage 中的 key
+      key: 'ahhh-front',
+      // 需要保存的模块
+      paths: ['category']
+    })
+  ]
 });
 
 export default store;
