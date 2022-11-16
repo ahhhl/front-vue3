5d478082ccbf6e0606b9a04f51f90214ffb2230c
ahhh <1208484386@qq.com>
2022-11-16 09:23:02 +0800

    通用组件infinite长列表,infinite结合waterfall实现长列表瀑布流效果

--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
-    <m-waterfall
-      class="px-1 w-full"
-      :data="pexelsList"
-      :nodeKey="'id'"
-      :column="isMobileTerminal ? 2 : 5"
-      :picturePreReading="true"
+    <m-infinite
+      v-model="loading"
+      :isFinished="isFinished"
+      @onLoad="getPexelsData"
     >
-      <template v-slot="{ item, width }">
-        <item-vue :data="item" :width="width"></item-vue>
-      </template>
-    </m-waterfall>
+      <m-waterfall
+        class="px-1 w-full"
+        :data="pexelsList"
+        :nodeKey="'id'"
+        :column="isMobileTerminal ? 2 : 5"
+        :picturePreReading="false"
+      >
+        <template v-slot="{ item, width }">
+          <item-vue :data="item" :width="width"></item-vue>
+        </template>
+      </m-waterfall>
+    </m-infinite>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'list'
+};
+</script>
 <script setup>
 import { getPexelsList } from '@/api/pexels';
 import { ref } from 'vue';
@@ -26,13 +37,26 @@ let query = {
   size: 20
 };
 
+const loading = ref(false);
+const isFinished = ref(false);
+
 const pexelsList = ref([]);
 
 const getPexelsData = async () => {
+  if (isFinished.value) {
+    return;
+  }
+  if (pexelsList.value.length) {
+    query.page++;
+  }
   const result = await getPexelsList(query);
-  pexelsList.value = result.list;
+  pexelsList.value = [...pexelsList.value, ...result.list];
+  if (pexelsList.value.length === result.total) {
+    isFinished.value = true;
+  }
+  loading.value = false;
 };
-getPexelsData();
+// getPexelsData();
 </script>
 
 <style lang="scss" scoped></style>
